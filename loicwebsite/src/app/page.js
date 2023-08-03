973b67900c053a78b4234243cb31ed818727818a
Loic <l.loic017@gmail.com>
2023-08-03 22:22:50 +0100

    setup

---
+++ loicwebsite/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main>
+            <p>Good Evening</p>
+            <p>Website coming soon</p>
+        </main>
+    );
+}
