34285b5f908694164e965b956924117088efadf6
Loic <l.loic017@gmail.com>
2023-08-06 22:57:35 +0100

    fix?

--- loic-website/app/home/page.jsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import HomeHero from "../../components/Home";
-
-export default function Home() {
-    return <HomeHero />;
-}
--- loic-website/app/socials/page.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Socials() {
-    return (
-        <section className="max-w-7xl mx-auto">
-            <h1>Soc</h1>
-        </section>
-    );
-}
